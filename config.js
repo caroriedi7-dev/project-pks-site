7ca41fd91cdee4280eabf6c410ef230c6fe90f25
caroriedi7-dev <caro.riedi7@gmail.com>
2026-08-02 19:39:14 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,6 @@ const DON_SHARED = {
   goal: 500000
 };
 
-const DON_THANKS = ["Valentine", "Marc", "Sofia", "Fabrice","Alain", "Christiane", "Claire"];
+const DON_THANKS = ["Valentine", "Marc", "Sofia", "Fabrice","Alain", "Christiane", "Claire", "Romain","Thierry","Louise","Michelle","Jean François","Samuel","Stephane","Michelle","Yann","Regiely","Laurent","Florian","Brigitte","Franziska","Patrick","Benoit","Bartosz","Kahina","Nicolas","Faustine","Mojdeh","Karine","Audrey","Gilda","Frederic","Cindy","Asmaa","Andre","Audrey","Nadia"];
+
+const DON_THANKS = [];
